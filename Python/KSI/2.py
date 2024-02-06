1d25541a17058c995103b0f832b48afadd79d11b
Martin Szollos <szollosmartin@gmail.com>
2024-02-06 15:26:09 +0100

    seminar konirc

--- Python/KSI/2.py
+++ Python/KSI/2.py
@@ -1,13 +1,13 @@
 import string
 
-translation_dict = dict([('a', '.-'), ('b', '-...'), ('c', '-.-.'), ('d', '-..'), ('e', '.'), ('f', '..-.'), ('g', '--.'), ('h', '....'), ('i', '..'), ('j', '.---'), ('k', '-.-'), ('l', '.-..'), ('m', '--'), ('n', '-.'), ('o', '---'), ('p', '.--.'), ('q', '--.-'), ('r', '.-.'), ('s', '...'), ('u', '..-'), ('v', '...-'), ('w', '.--'), ('x', '-..-'), ('y', '-.--'), ('z', '--..'), (' ', ''), ('/', '-..-.'), ('-', '-....-'), ('.', '.-.-.-'), (',', '--..--'), (' ', ''), ("1", ".----"), ("2", "..---"), ("3", "...--"), ("4", "....-"), ("5", "....."), ("6", "-...."), ("7", "--..."), ("8", "---.."), ("9", "----."), ("0", "-----")])
+translation_dict = dict([('a', '.-'), ('b', '-...'), ('c', '-.-.'), ('d', '-..'), ('e', '.'), ('f', '..-.'), ('g', '--.'), ('h', '....'), ('i', '..'), ('j', '.---'), ('k', '-.-'), ('l', '.-..'), ('m', '--'), ('n', '-.'), ('o', '---'), ('p', '.--.'), ('q', '--.-'), ('r', '.-.'), ('s', '...'),('t', '-'), ('u', '..-'), ('v', '...-'), ('w', '.--'), ('x', '-..-'), ('y', '-.--'), ('z', '--..'), (' ', ''), ('/', '-..-.'), ('-', '-....-'), ('.', '.-.-.-'), (',', '--..--'), (' ', ''), ("1", ".----"), ("2", "..---"), ("3", "...--"), ("4", "....-"), ("5", "....."), ("6", "-...."), ("7", "--..."), ("8", "---.."), ("9", "----."), ("0", "-----")])
 
 alphabet = {}
 morse = {}
 
 for key, value in translation_dict.items():
     alphabet[key] = value
-    alphabet[value] = key
+    morse[value] = key
 
 def encode(plaintext: str) -> str:
     plaintext = plaintext.lower()
@@ -22,11 +22,11 @@ def decode(morse_text: str) -> str:
     plain_text = ""
     for i in range(len(morse_array)):
         current_element = morse_array[i]
-        plain_text += alphabet[current_element]
-    plain_text = plain_text[:-5]
+        plain_text += morse[current_element]
+    plain_text = plain_text[:-3]
     return plain_text
 
-# print(decode(encode("Najit vsechny bugy je casto problem")))
-# print(decode(encode("Uspet neni lehke, ale je to sladka tecka.")))
-# print(decode(encode("Do translation dict jsou pridany vsechny specialni znaky a cisla, ktera budete potrebovat. To stejne nerikam o malych pismenech.")))
+print(decode(encode("Najit vsechny bugy je casto problem")))
+print(decode(encode("Uspet neni lehke, ale je to sladka tecka.")))
+print(decode(encode("Do translation dict jsou pridany vsechny specialni znaky a cisla, ktera budete potrebovat. To stejne nerikam o malych pismenech.")))
 print(encode("ab c")) # .-/-...//-.-.///
