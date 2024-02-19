66304fe715a7a57c5a12e23adcd375ceffe1a541
Martin Szollos <szollosmartin@gmail.com>
2024-02-19 16:40:12 +0100

    Notebook

---
+++ programy_C++/seminar/set.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct sit
+{
+    vector<int> nieco(0,1000000);
+    void add (vector<int> nieco,int n);
+    void del (vector<int> nieco,int n);
+    bool get (vector<int> nieco,int n);
+};
+
+void sit::add (vector<int> nieco,int n){
+    nieco[i]=1;
+}
+void sit::del(vector<int> nieco,int n){
+    nieco[i]=0
+}
+bool sit::get(vector<int> nieco,int n){
+    if(nieco[i]==0)
+    return false;
+    else 
+    return true;
+}
+
+
+int main(){
+    sit s();
+
+}
