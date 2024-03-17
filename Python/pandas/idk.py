d2b5fb0a3211c7fd70705a6ba124677613f9421b
kokorokjo <szollosmartin@gmail.com>
2024-03-17 19:50:08 +0100

    numpy

---
+++ Python/pandas/idk.py
@@ -0,0 +1,7 @@
+import numpy 
+  
+# create an numpy array with 5 elements 
+data = numpy.array([1, 2, 3, 4, 5]) 
+  
+# display 
+data 
--- programy_C++/IO/IO1.cpp
+++ programy_C++/IO/IO1.cpp
@@ -1,166 +1,22 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-    int n, k, ke, l, b = 0, pomocna, right = 0, left = 0, pright, poradie = 0, pleft, hodnota, p = 0;
-    cin >> n >> k >> l;
-    int hlavny[n];
-    vector<int> postup(0);
-    for (int i = 0; i < n; i++)
-    {
-        cin >> hlavny[i];
-    }
-    ke = hlavny[k - 1];
-    for (int i = 0; i < n; i++)
-    {
-        if (hlavny[i] < ke)
-        {
-            left++;
-        }
-        if (hlavny[i] > ke)
-        {
-            right++;
-        }
-    }
-    if (left >= k)
-    {
-        cout << "NIE" <<  endl;
-        return 0;
-    }
-    if (right + k > n)
-    {
-        cout << "NIE" <<  endl;
-        return 0;
-    }
-    right=0;
-    left=0;
-    while (poradie < k - 1)
-    {
+int main() {
+int N, D;
+cin >> N >> D;
+vector<int> A(N), P(N+1,0);
+for (int n=0; n<N; ++n) { int a; cin >> a; A[n] = (a>=D ? 1 : -1); P[n+1] = P[n] + A[n]; }
 
-        if (hlavny[poradie] > ke)
-        {
-            pleft = poradie;
-            pright = k - 2;
-            if (pleft != pright)
-            {
-                postup.push_back(pleft + 1);
-                postup.push_back(pright + 1);
-                p = p + 2;
-            }
-            while (pleft < pright)
-            {
-                swap(hlavny[pleft], hlavny[pright]);
-                pleft++;
-                pright--;
-
-            }
-            swap(hlavny[k - 2], hlavny[k]);
-            postup.push_back(k - 1);
-            postup.push_back(k + 1);
-            p = p + 2;
-            pleft = k;
-            pright = n - 1 - right;
-            if (pright != pleft)
-            {
-                postup.push_back(pleft + 1);
-                postup.push_back(pright + 1);
-                p = p + 2;
-            }
-            while (pleft < pright)
-            {
-
-                swap(hlavny[pleft], hlavny[pright]);
-                pright--;
-                pleft++;
-            }
-            right++;
-        }
-        else
-        {
-            poradie++;
-        }
-    }
-
-    for (int i = 0; i < k - 1; i++)
-    {
-        hodnota = hlavny[i];
-        pomocna = i;
-        for (int f = i; f < k - 1; f++)
-        {
-            if (hodnota > hlavny[f])
-            {
-                hodnota = hlavny[f];
-                pomocna = f;
-            }
-        }
-        pleft = i;
-        pright = pomocna;
-        if (pright != pleft)
-        {
-            postup.push_back(pleft + 1);
-            postup.push_back(pright + 1);
-            p = p + 2;
-        }
-        while (pleft < pright)
-        {
-            swap(hlavny[pleft], hlavny[pright]);
-            pright--;
-            pleft++;
-
-        }
-    }
-    hodnota = hlavny[k];
-    for (int i = k; i < n - 1; i++)
-    {
-        hodnota = hlavny[i];
-        pomocna = i;
-        for (int f = i; f < n; f++)
-        {
-            if (hodnota > hlavny[f])
-            {
-                hodnota = hlavny[f];
-                pomocna = f;
-            }
-
-        }
-
-        pleft = i;
-        pright = pomocna;
-        if (pright != pleft)
-
-        {
-            postup.push_back(pleft + 1);
-            postup.push_back(pright + 1);
-            p = p + 2;
-        }
-        while (pleft < pright)
-        {
-            swap(hlavny[pleft], hlavny[pright]);
-            pright--;
-            pleft++;
-        }
-    }
-
-
-
-    if(l == 0){
-        cout<<"ANO"<<endl;
-    }
-    else{
-        if(p<l){
-            cout << "ANO" << endl;
-                p = p / 2;
-                cout << p << endl;
-                p = p * 2;
-                for (int i = 0; i < p; i = i + 2)
-                {
-
-                    cout << postup[i] << " " << postup[i + 1] << endl;
-                }
-        }
-        else{
-            cout<<"NIE"<<endl;
-        }
-    }
+vector<int> videl(2*N+1,0); // videl[N+i] hovorí, koľko už spracovaných prvkov P malo hodnotu presne i
+int leq = 0; // počet už spracovaných prvkov P ktoré boli menšie alebo rovné ako aktuálny
+int odpoved = 0;
+for (int n=1; n<=N; ++n) {
+// pridáme prvok P[n-1] medzi už videné, aj medzi prvky <= ako aktuálny
+++videl[ N+P[n-1] ];
+++leq;
+// posunieme aktuálny prvok na P[n], čím buď pribudnú alebo ubudnú skôr spracované <= od neho
+if (P[n] == P[n-1] + 1) leq += videl[ N+P[n] ]; else leq -= videl[ N+P[n-1] ];
+odpoved += leq;
+}
+cout << odpoved << endl;
 }
