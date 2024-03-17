153de7c4c6e83e402c8b88d7b2c7af5660a67928
kokorokjo <szollosmartin@gmail.com>
2024-03-17 20:26:16 +0100

    numpy+pandas

---
+++ Python/baseData/test.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+df = pd.read_csv('/home/martin/Desktop/Programy/Python/baseData/data.csv')
+
+print(df) 
--- Python/pandas/idk.py
+++
@@ -1,7 +0,0 @@
-import numpy 
-  
-# create an numpy array with 5 elements 
-data = numpy.array([1, 2, 3, 4, 5]) 
-  
-# display 
-data 
