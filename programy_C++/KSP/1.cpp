24d407546ed140fcb8c72b85f45eaaa300130a2d
kokorokjo <szollosmartin@gmail.com>
2024-04-15 21:52:55 +0200

    ksp

--- programy_C++/KSP/1.cpp
+++ programy_C++/KSP/1.cpp
@@ -1,31 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct point{
-    int x,y;
-};
-
-
-
 
 int main(){
-
     int n;
+    cin >> n;
     int s,v;
-    cin >> n >> s >> v;
-    vector<point> a(n);
+    cin >> s >> v;
+    vector<int> a(n);
     for(int i=0;i<n;i++){
-        cin >> a[i].x >> a[i].y;
-    }
-    vector<vector<int>> inside(n);
-    for(int i=0;i<n;++i){
-        for(int j=i+1;j<n;++j){
-            if(a[i].x>=a[j].x && a[i].x+s<a[j].x){
-                inside[i].push_back(j);
-                inside[j].push_back(i);
-            }
-        }
+        cin >> a[i];
+        int x;
+        cin >> x;
     }
     
+    int max = 1;
+    int last = a[0];
+    for(int i=0;i<n;i++){
+        if(s+last>a[i]){
+            
+            continue;
+        }
+        max++;
+        last=a[i];
+        
+    }
+    cout<<max<<endl;
 
+    
 }
--- programy_C++/KSP/2.cpp
+++ programy_C++/KSP/2.cpp
@@ -1,75 +1,70 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int binarySearch(vector<pair<string,int>> hlavny,int start,int end,string to){
+    int stred=(end+start)/2;
+    if(hlavny[end].first==to) return end;
+    if(hlavny[start].first==to) return start;
+
+
+    if(hlavny[stred].first==to) return stred;
+    else if(hlavny[stred].first>to) stred = binarySearch(hlavny,start,stred,to);
+    else stred = binarySearch(hlavny,stred,end,to);
+    return stred;
+}
 
 
 int main(){
 
-    int n;
-    cin >> n;
-   
-    for(int vsetko=0;vsetko<n;vsetko++){
-        int vysledok=INT_MAX;
-        string hlavny;
-        cin>>hlavny;
-        string pomocny=hlavny;
-        vector<pair<int,char>> cisielka;
-        vector<int> cisielka2(123,-1);
-        cisielka.push_back(make_pair(1,hlavny[0]));
-        cisielka2[hlavny[0]]=1;
-        pomocny[0]='1';
-        for(int i=0;i<hlavny.size();i++){
-            if(cisielka2[hlavny[i]]==-1){
-                cisielka.push_back(make_pair(0,hlavny[i]));
-                cisielka2[hlavny[i]]=0;
-                pomocny[i]='0';
-                break;
-            }
-        }
-        //  cout<<"este"<<pomocny<<endl;
-        for(int i=0;i<hlavny.size();i++){
-            int pomocne=-1;
-            
-                if(cisielka2[hlavny[i]]!=-1){
-                    pomocne=cisielka2[hlavny[i]];
-                }
-            if(pomocne==-1){
-                cisielka.push_back(make_pair(cisielka.size(),hlavny[i]));
-                cisielka2[hlavny[i]]=cisielka.size();
-                pomocny[i]=cisielka.size()+48-1;
-            }
-            else{
-                pomocny[i]=pomocne+48;
-            }
+    
+int t;
+cin>>t;
+while(t>0){
+t--;
 
-        }
-        //  cout<<pomocny<<endl;
-
-        
-        for(int sustava=cisielka.size();sustava<=10;sustava++){
-            if(sustava==1){
-                continue;
-            }
-            int pocet=0;
-            for(int i=0;i<pomocny.size();i++){
-                pocet*=sustava;
-                pocet+=pomocny[i]-48;
-            }
-            // cout<<sustava<<" "<<pocet<<endl;
-            if(pocet<vysledok){
-                vysledok=pocet;
-            }
-        }
-        cout<<vysledok<<endl;
+int h,k;
+int u=1;
+int pocet=0;
+int vysledok=0;
+// set<string> mena;
+// vector<int> hlavny;
+// pair<int,> spojovac;
+vector<pair<string,int>> hlavny;
 
 
+cin>>h;
+string m;
+getline(cin,m);
 
+for(int i=0;i<h;++i){
+    string p;
+    getline(cin,p);
+    hlavny.push_back({p,0});
 
+}
+sort(hlavny.begin(),hlavny.end());
 
+cin>>k;
+string g;
+getline(cin,g);
 
-
+for(int i=0;i<k;++i){
+    string p;
+    getline(cin,p);
+    int f = binarySearch(hlavny,0,hlavny.size()-1,p);
+    if(hlavny[f].second == u) continue;
+    hlavny[f].second++;
+    pocet++;
+    if(pocet>=h){
+        vysledok++;
+        u++;
+        pocet=1;
+        hlavny[f].second++;
     }
+    
 
 
-
+}
+cout<<vysledok<<endl;
+}
 }
