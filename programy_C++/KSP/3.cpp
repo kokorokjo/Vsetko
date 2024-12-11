4d9f6546bd9b6cbac3610fa16dbb7b2ab401f188
kokorokjo <szollosmartin@gmail.com>
2024-12-11 19:55:29 +0100

    funkcnaTrojka

--- programy_C++/KSP/3.cpp
+++ programy_C++/KSP/3.cpp
@@ -1,28 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-vector <pair<int,int>> v(1,make_pair(-1,-1));
-
-void prepis(int k){
-    
-    while (k>1){
-        if(v[k/2].first == -1 || v[k/2].first>v[k].first){
-            v[k/2].first = v[k].first;
-        }
-        if(v[k/2].second == -1 || v[k/2].second>v[k].second){
-            v[k/2].second = v[k].second;
-        }
-        k = k/2;
-    }
-}
+typedef long long ll;
 
 int main() {
-    int n,t,p;
+    long long n,t,p;
     cin>>n>>t>>p;
-    int pred=0;
-    int sucet=0;
-    for(int i=0;i<n;i++){
-        int a,b;
+    long long pred=0;
+    long long sucet=0;
+    for(long long i=0;i<n;i++){
+        long long a,b;
         cin>>a>>b;
         if(a<=b*t){
             p--;
@@ -31,9 +17,13 @@ int main() {
         else{
             pred++;
         }
-        if(p=0){
+        if(p==0){
             break;
         }
+    }
+    if(p!=0){
+        cout<<-1<<endl;
+        return 0;
     }
     cout<<sucet<<endl;
     
